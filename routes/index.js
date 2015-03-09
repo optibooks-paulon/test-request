60f3fdddd300661cb903f9b4fc4d66a2b2749197
optibooks-paulon <paulon.zervoulakus@optibooks.com>
2015-03-09 15:10:48 +1100

    added callback route, after a user connected and chooses organization from xero

--- routes/index.js
+++ routes/index.js
@@ -9,15 +9,13 @@ router.get('/', function(req, res) {
 });
 
 router.get('/oauth', function(req, res) {
- 
-
   var o = new oauth.OAuth(
     'https://api.xero.com/oauth/RequestToken',
     'https://api.xero.com/oauth/AccessToken',
     'UZHRDQT9PBL308HIABNBCJCUGNKQYE',
     'VM4LTY2HWG43XGEHHFI5GBOJOTMGIM',
-    '1.0A',
-    null,
+    '1.0',
+    'http://localhost:3000/oauth/access',
     'HMAC-SHA1'
   );
   console.log('before');
@@ -50,10 +48,18 @@ router.get('/oauth', function(req, res) {
       saveSecret
       ], function(err, data) {
         console.log('request token and secret has been saved to redis');
-        res.json({token: token, secret: secret});
+        var url = 'https://api.xero.com/oauth/Authorize';
+        url += '?oauth_token=' + token;
+        url += '&redirect_uri=' + 'http://localhost:3000/oauth/access';
+        return res.json({token: token, secret: secret, url: url});
       });    
   });
   console.log('after');
 
+});
+
+router.get('/oauth/access', function(req, res) {
+  console.log('****** Received GET /oauth/access', req.query);
+  res.render('index', { title: 'Connected' });
 });
 module.exports = router;
